d0ffc47cea14706772502bce89d426b4841d438e
mpro34 <mpro34@gmail.com>
2015-04-02 22:18:45 -0700

    Added a new assignment for objects in c++

---
+++ Objects.cpp
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <iostream>
+#include <cstdlib>
+
+/*
+ * This File contains MIT C++ Assignment 3
+ */
+ 
+ //2.1
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ int main() {
+	 std::cout << "Hello, world!" << std::endl;
+	 return 0;
+ }
